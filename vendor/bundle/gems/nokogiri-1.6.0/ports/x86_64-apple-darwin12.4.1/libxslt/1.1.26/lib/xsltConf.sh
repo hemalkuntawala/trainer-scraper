29387e71ac824072f1a18203b396052c630e63d3
Hemal Kuntawala <hemal.kuntawala@gmail.com>
2013-11-03 13:34:49 +0000

    Removed vendor

--- vendor/bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin12.4.1/libxslt/1.1.26/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/hemal/Development/trainer-scraper/vendor/bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin12.4.1/libxslt/1.1.26/lib"
-XSLT_LIBS="-lxslt  -L/Users/hemal/Development/trainer-scraper/vendor/bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin12.4.1/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
-XSLT_INCLUDEDIR="-I/Users/hemal/Development/trainer-scraper/vendor/bundle/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin12.4.1/libxslt/1.1.26/include"
-MODULE_VERSION="xslt-1.1.26"
